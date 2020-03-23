a08314de6d0dc7bdf9d22ff7fe1a9217625d097d
BransonGitomeh <gitomehbranson@gmail.com>
2020-03-24 02:08:54 +0300

    mesh change transform positions on click

--- scripts/steve.gd
+++ scripts/steve.gd
@@ -4,12 +4,22 @@ var velocity = Vector3(0,0,0)
 const SPEED = 5
 onready var playerMesh = get_node("/root/level/steve/Sphere")
 onready var obstacleMesh = get_node("/root/level/obstacle/MeshInstance")
+onready var camera = get_node("/root/level/Camera")
 
 func _ready():
 	printt("hello ", velocity) 
-	
+
 func _physics_process(delta):
 	obstacleMesh.rotate_y(deg2rad(-3))
+	playerMesh.rotate_y(deg2rad(3))
+	
+	if Input.is_action_pressed("click"):
+        var position2D = get_viewport().get_mouse_position()
+        var dropPlane  = Plane(Vector3(0, 0, 10), 3)
+        var position3D = dropPlane.intersects_ray(camera.project_ray_origin(position2D),camera.project_ray_normal(position2D))
+        # print(position3D)
+        playerMesh.transform.origin = position3D
+		
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_right"):
