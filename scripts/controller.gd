7cf80b0a84a3484df4cdce77ad66d5d146bcbf78
BransonGitomeh <gitomehbranson@gmail.com>
2020-03-24 13:02:47 +0300

    higher and lower controlls

--- scripts/controller.gd
+++ scripts/controller.gd
@@ -12,6 +12,7 @@ func _physics_process(delta):
 	# clicking on an object then hitting ctrl + space should bring the camera xyz units near the selected object to
 	# put it in focus
 	
+	# right left direct movement
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_right"):
@@ -21,6 +22,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = lerp(velocity.x,0,.1)
 		
+	# front back movement
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
 		velocity.z = 0
 	elif Input.is_action_pressed("ui_up"):
@@ -30,4 +32,13 @@ func _physics_process(delta):
 	else:
 		velocity.z = lerp(velocity.x,0,.1)
 		
+	# higer lower movement
+	if Input.is_action_pressed("higher") and Input.is_action_pressed("lower"):
+		velocity.y = 0
+	elif Input.is_action_pressed("higher"):
+		velocity.y = SPEED
+	elif Input.is_action_pressed("lower"):
+		velocity.y = -SPEED
+	else:
+		velocity.y = lerp(velocity.x,0,.1)
 	move_and_slide(velocity)
